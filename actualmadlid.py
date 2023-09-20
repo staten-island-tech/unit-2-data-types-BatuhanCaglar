403c8d5911d7d343e959e7932ecd268f845b4f2e
BatuhanCaglar <fbmanbc@gmail.com>
2023-09-20 14:01:18 -0400

    commit tip

--- actualmadlid.py
+++ actualmadlid.py
@@ -1,8 +1,30 @@
 
-adjective = "bummy"
-verb1=("cooking")
-verb2=("running")
-verb3=("baking")
+#Insert all choices within the parantheses and quotations 
 
-print(f"you are very {adjective}")
+#Firstly, insert one adjective below
+adjective = ("funny")
+
+#Secondly, insert 3 verbs of your choosing below,
+verb1=("baking")
+verb2=("climb")
+verb3=("jump")
+
+#Thirdly, insert 2 of your favorite celebrity
+celebrity1=("Tom Cruise")
+celebrity2=("Christian Bale")
+
+#Next, include a number of your picking
+number=("9")
+
+#After, please choose any 3 forms of food
+food1=("ice cream")
+food2=("hamburgers")
+food3=("poptarts")
+
+#Finally, choose a vehicle
+vehicle=("Koenigsegg Agera")
+
+
+#Your madlib will appear in the terminal below
+print(f"You're driving down the highway at mach 10 in a {vehicle} and suddenly {celebrity1} appears in front of your car eating {food1}. Their also {verb1} with {celebrity1} and both celebrities look(ed) {adjective}. They summon {number} {food2} to {verb2} your car. You {verb3} with {food3} and escape them and their army  ")
 
---
+++ tips.py
@@ -0,0 +1,20 @@
+
+temp = 65
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else: temp < 68
+print('cold')
+
+
+def oddeven():
+    if (x==0):
+        print("even")
+    else :
+        print("odd")
+
+#insert number
+value = (4)
+
+x = (4/2)
