106fc6b28ef11d6f83829bbe9b53e34a59a2f99e
BatuhanCaglar <fbmanbc@gmail.com>
2023-10-03 14:01:04 -0400

    more stuff

--- GCF.py
+++ GCF.py
@@ -1,13 +1,13 @@
-num=int(input("Please enter a number"))
-num2=int(input("Please enter a second number"))
-
+x = input("Enter a number:")
+y = input("Enter a second:")
+list = []
 def gcf():
-  for i in range(num, 0, -1):
-    if num%i==0 and num2%i==0:
-      print(i)
-      max(i)
+  for i in range(x,y):
+    if x%i(2) == 0 and y%i(2) == 0:
+      print (i) in list
+      gcf == max(list)
+      print ("gcf")
 gcf()
-
 
 
     
